66e23ebd46a97f9326438c337620ed6264568238
Caio Soares Silva <07soaresscaio@gmail.com>
2025-09-13 12:48:07 -0300

    feat: add classe TipoServico

---
+++ tipo_servico.py
@@ -0,0 +1,15 @@
+class TipoServico():
+    def __init__(self, nome: str):
+        self.__nome = None
+
+        if isinstance(nome, str):
+            self.__nome = nome
+        
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome):
+        if isinstance(nome, str):
+            self.__nome = nome
