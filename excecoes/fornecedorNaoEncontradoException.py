8bcb2d6335ad12a6bc397ec266418233f86d7fc3
igortcr2909 <igortcr2909@gmail.com>
2025-10-13 16:19:07 -0300

    Delete excecoes directory
    Deletando os arquivos de exceções pois encontrei outras alternativas

--- excecoes/fornecedorNaoEncontradoException.py
+++
@@ -1,3 +0,0 @@
-class FornecedorNaoEncontradoException(Exception):
-    def __init__(self):
-        super().__init__("Fornecedor não encontrado!")
--- excecoes/lojaNaoEncontradaException.py
+++
@@ -1,3 +0,0 @@
-class LojaNaoEncontradaException(Exception):
-    def __init__(self):
-        super().__init__('A Loja Não foi Encontrada')
