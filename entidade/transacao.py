3cc61d581ff7b7401c8c016d3688f887f0da4cc3
igortcr2909 <igortcr2909@gmail.com>
2025-10-13 16:34:39 -0300

    Update transacao.py

--- entidade/transacao.py
+++ entidade/transacao.py
@@ -1,30 +1,55 @@
+from abc import ABC
 from .veiculo import Veiculo
+from datetime import datetime
 
-
-class Transacao:
-    def __init__(self, veiculo: Veiculo, valor: float):
+class Transacao(ABC):
+    __contador_id = 0
+    
+    def __init__(self, veiculo: Veiculo, valor: float, data=None):
         self.__veiculo = None
         self.__valor = None
-
+        self.__data = None
+        
+        Transacao.__contador_id += 1
+        self.__id = Transacao.__contador_id
+        
         if isinstance(veiculo, Veiculo):
             self.__veiculo = veiculo
         if isinstance(valor, float):
             self.__valor = valor
-
+        
+        if data is None:
+            self.__data = datetime.now()
+        elif isinstance(data, datetime):
+            self.__data = data
+    
+    @property
+    def id(self):
+        return self.__id
+    
     @property
     def veiculo(self):
         return self.__veiculo
-
+    
     @veiculo.setter
     def veiculo(self, veiculo: Veiculo):
         if isinstance(veiculo, Veiculo):
             self.__veiculo = veiculo
-
+    
     @property
     def valor(self):
         return self.__valor
-
+    
     @valor.setter
     def valor(self, valor: float):
         if isinstance(valor, float):
             self.__valor = valor
+    
+    @property
+    def data(self):
+        return self.__data
+    
+    @data.setter
+    def data(self, data: datetime):
+        if isinstance(data, datetime):
+            self.__data = data
