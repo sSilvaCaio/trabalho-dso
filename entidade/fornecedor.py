b7589f0d48087ba4227719e609dd3fbb3cce59d7
igortcr2909 <igortcr2909@gmail.com>
2025-10-13 16:33:44 -0300

    Update fornecedor.py

--- entidade/fornecedor.py
+++ entidade/fornecedor.py
@@ -1,18 +1,26 @@
 from .pessoa import Pessoa
 
-
 class Fornecedor(Pessoa):
     def __init__(self, nome: str, telefone: str, idade: int, sexo: str, cnpj: str):
         super().__init__(nome, telefone, idade, sexo)
         self.__cnpj = None
-
         if isinstance(cnpj, str):
             self.cnpj = cnpj
-
+    
+    def __str__(self):
+        return (
+            f"--- Fornecedor ---\n"
+            f"  Nome: {self.nome}\n"
+            f"  CNPJ: {self.cnpj}\n"
+            f"  Telefone: {self.telefone}\n"
+            f"  Idade: {self.idade}\n"
+            f"  Sexo: {self.sexo}\n"
+        )
+    
     @property
     def cnpj(self):
         return self.__cnpj
-
+    
     @cnpj.setter
     def cnpj(self, cnpj: str):
         if isinstance(cnpj, str):
