aecf9dd4f27c35e6687d5df3074f22e12348527b
igortcr2909 <igortcr2909@gmail.com>
2025-10-13 16:29:16 -0300

    Update controlador_fornecedor.py

--- controlador/controlador_fornecedor.py
+++ controlador/controlador_fornecedor.py
@@ -1,103 +1,110 @@
 from entidade.fornecedor import Fornecedor
 from limite.tela_fornecedor import TelaFornecedor
-from excecoes.fornecedorNaoEncontradoException import FornecedorNaoEncontradoException
 
 class ControladorFornecedor:
-    def __init__(self):
-        self.__fornecedores = []
-        self.__tela_fornecedor = TelaFornecedor(self)
-
+    def __init__(self, controlador_principal):
+        self.__tela = TelaFornecedor(self)
+        self.__controlador_principal = controlador_principal
+    
+    @property
+    def tela(self):
+        return self.__tela
+    
+    @property
+    def controlador_principal(self):
+        return self.__controlador_principal
+    
+    def abre_tela_opcoes(self):
+        switcher = {1: self.cadastra_fornecedor, 2: self.lista_fornecedores, 3: self.altera_fornecedor, 4: self.deleta_fornecedor}
+        
+        while True:
+            opcao = self.tela.mostra_tela_opcoes()
+            if opcao == 0:
+                break
+            
+            funcao = switcher.get(opcao)
+            if funcao:
+                funcao()
+            else:
+                self.tela.mostra_mensagem_erro("Opção inválida.")
+    
+    def cadastra_fornecedor(self):
+        while True:
+            dados_fornecedor = self.tela.mostra_tela_cadastro()
+            if not dados_fornecedor:
+                return None
+            
+            if self.busca_fornecedor_por_cnpj(dados_fornecedor['cnpj']):
+                self.tela.mostra_mensagem_erro("Já existe um fornecedor com este CNPJ!")
+                continue
+            
+            novo_fornecedor = Fornecedor(
+                nome=dados_fornecedor["nome"],
+                telefone=dados_fornecedor["telefone"],
+                idade=dados_fornecedor["idade"],
+                sexo=dados_fornecedor["sexo"],
+                cnpj=dados_fornecedor["cnpj"],
+            )
+            
+            self.controlador_principal.loja.fornecedores_cadastrados.append(novo_fornecedor)
+            self.tela.mostra_mensagem("Fornecedor cadastrado com sucesso.")
+            return novo_fornecedor
+    
+    def lista_fornecedores(self):
+        fornecedores = self.controlador_principal.loja.fornecedores_cadastrados
+        if not fornecedores:
+            self.tela.mostra_mensagem("Nenhum fornecedor cadastrado.")
+            return None
+        
+        self.tela.mostra_tela_lista(fornecedores)
+    
+    def deleta_fornecedor(self):
+        while True:
+            cnpj = self.tela.mostra_tela_deletar()
+            if not cnpj:
+                return None
+            
+            fornecedor = self.busca_fornecedor_por_cnpj(cnpj)
+            if not fornecedor:
+                self.tela.mostra_mensagem_erro("Não existe fornecedor com este CNPJ.")
+                continue
+            
+            removido_com_sucesso = self.deleta_fornecedor_por_objeto(fornecedor)
+            if removido_com_sucesso:
+                self.tela.mostra_mensagem('Fornecedor deletado.')
+                return True
+            else:
+                self.tela.mostra_mensagem_erro('Erro ao deletar fornecedor.')
+                return False
+    
+    def altera_fornecedor(self):
+        while True:
+            novos_dados = self.tela.mostra_tela_alteracao()
+            if not novos_dados:
+                return None
+            
+            fornecedor = self.busca_fornecedor_por_cnpj(novos_dados['cnpj'])
+            if not fornecedor:
+                self.tela.mostra_mensagem_erro('Não existe fornecedor com este CNPJ.')
+                continue
+            
+            if novos_dados['nome'] != ' ': fornecedor.nome = novos_dados["nome"]
+            if novos_dados['telefone'] != ' ': fornecedor.telefone = novos_dados["telefone"]
+            if novos_dados['idade'] != ' ': fornecedor.idade = novos_dados["idade"]
+            if novos_dados['sexo'] != ' ': fornecedor.sexo = novos_dados["sexo"]
+            
+            self.tela.mostra_mensagem('Fornecedor alterado.')
+            return fornecedor
+    
+    def deleta_fornecedor_por_objeto(self, fornecedor_para_deletar: Fornecedor):
+        if fornecedor_para_deletar and isinstance(fornecedor_para_deletar, Fornecedor):
+            if fornecedor_para_deletar in self.__controlador_principal.loja.fornecedores_cadastrados:
+                self.__controlador_principal.loja.fornecedores_cadastrados.remove(fornecedor_para_deletar)
+                return True
+        return False
+    
     def busca_fornecedor_por_cnpj(self, cnpj):
-        for fornecedor in self.__fornecedores:
+        for fornecedor in self.controlador_principal.loja.fornecedores_cadastrados:
             if fornecedor.cnpj == cnpj:
                 return fornecedor
-        raise FornecedorNaoEncontradoException
-
-    def cadastra_fornecedor(self):
-        try:
-            self.__tela_fornecedor.mostra_tela_inicial()
-            dados_fornecedor = self.__tela_fornecedor.mostra_tela_cadastro()
-            try:
-                self.busca_fornecedor_por_cnpj(dados_fornecedor['cnpj'])
-                self.__tela_fornecedor.mostra_mensagem("ERRO: Fornecedor já cadastrado!")
-                return None
-            except FornecedorNaoEncontradoException:
-                pass
-            novo_fornecedor = Fornecedor(
-                dados_fornecedor["nome"],
-                dados_fornecedor["telefone"],
-                dados_fornecedor["idade"],
-                dados_fornecedor["sexo"],
-                dados_fornecedor["cnpj"]
-            )
-            self.__fornecedores.append(novo_fornecedor)
-            self.__tela_fornecedor.mostra_mensagem("Fornecedor cadastrado com sucesso!")
-            return novo_fornecedor
-        except TypeError as e:
-            return self.__tela_fornecedor.mostra_mensagem_TypeError(e.args)
-        except Exception as e:
-            return self.__tela_fornecedor.mostra_mensagem_Exception(e)
-
-    def lista_fornecedores(self):
-        if len(self.__fornecedores) == 0:
-            self.__tela_fornecedor.mostra_mensagem("Nenhum fornecedor cadastrado!")
-            return
-        for fornecedor in self.__fornecedores:
-            dados_fornecedor = {
-                "nome": fornecedor.nome,
-                "telefone": fornecedor.telefone,
-                "idade": fornecedor.idade,
-                "sexo": fornecedor.sexo,
-                "cnpj": fornecedor.cnpj
-            }
-            self.__tela_fornecedor.mostra_dados_fornecedor(dados_fornecedor)
-
-    def altera_fornecedor(self):
-        try:
-            cnpj = self.__tela_fornecedor.seleciona_fornecedor()
-            fornecedor = self.busca_fornecedor_por_cnpj(cnpj)
-            novos_dados = self.__tela_fornecedor.mostra_tela_alteracao()
-            if novos_dados["nome"]:
-                fornecedor.nome = novos_dados["nome"]
-            if novos_dados["telefone"]:
-                fornecedor.telefone = novos_dados["telefone"]
-            if novos_dados["idade"]:
-                fornecedor.idade = novos_dados["idade"]
-            if novos_dados["sexo"]:
-                fornecedor.sexo = novos_dados["sexo"]
-            self.__tela_fornecedor.mostra_mensagem("Fornecedor alterado com sucesso!")
-            return True
-        except TypeError as e:
-            return self.__tela_fornecedor.mostra_mensagem_TypeError(e.args)
-        except Exception as e:
-            return self.__tela_fornecedor.mostra_mensagem_Exception(e)
-
-    def deleta_fornecedor(self):
-        try:
-            cnpj = self.__tela_fornecedor.seleciona_fornecedor()
-            fornecedor = self.busca_fornecedor_por_cnpj(cnpj)
-            self.__fornecedores.remove(fornecedor)
-            self.__tela_fornecedor.mostra_mensagem("Fornecedor deletado com sucesso!")
-            return True
-        except TypeError as e:
-            return self.__tela_fornecedor.mostra_mensagem_TypeError(e.args)
-        except Exception as e:
-            return self.__tela_fornecedor.mostra_mensagem_Exception(e)
-
-    def abre_tela_opcoes(self):
-        switcher = {
-            0: 'break',
-            1: self.cadastra_fornecedor,
-            2: self.lista_fornecedores,
-            3: self.altera_fornecedor,
-            4: self.deleta_fornecedor
-        }
-        while True:
-            opcao = self.__tela_fornecedor.mostra_tela_opcoes()
-            funcao_escolhida = switcher.get(opcao)
-            if funcao_escolhida == 'break':
-                break
-            elif funcao_escolhida:
-                funcao_escolhida()
-            else:
-                self.__tela_fornecedor.mostra_mensagem("Opção inválida!")
+        return None
