869336e60f26ee7b63eb804e7b5e369bdbf18246
Igortcr2909 <igortcr2909@gmail.com>
2025-11-18 18:45:47 -0300

    Refactor ClienteDAO to use BaseDAO and change key type

--- dao/cliente_dao.py
+++ dao/cliente_dao.py
@@ -1,22 +1,22 @@
-from abstract_dao import AbstractDAO
+from dao.abstract_dao import BaseDAO
 from entidade.cliente import Cliente
 
-class ClienteDAO(AbstractDAO):
+class ClienteDAO(BaseDAO):
     def __init__(self):
         super().__init__("clientes.pkl")
     
     def add(self, cliente: Cliente):
         if isinstance(cliente, Cliente) and isinstance(cliente.cpf, str) and (cliente is not None):
-            super().add(cliente.cpf, cliente)        
+            return super().add(cliente.cpf, cliente)        
     
     def update(self, cliente: Cliente):
         if isinstance(cliente, Cliente) and isinstance(cliente.cpf, str) and (cliente is not None):
-            super().update(cliente.cpf, cliente)
+            return super().update(cliente.cpf, cliente)
 
-    def get(self, key: int):
-        if isinstance(key, int):
+    def get(self, key: str):
+        if isinstance(key, str):
             return super().get(key)
     
-    def remove(self, key: int):
-        if isinstance(key, int):
-            super().remove(key)
+    def remove(self, key: str):
+        if isinstance(key, str):
+            return super().remove(key)
